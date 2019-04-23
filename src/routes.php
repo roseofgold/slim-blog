a66bfaab88654e4a802638c5c626149abce24aca
Melinda S <roseofgold@gmail.com>
2019-04-22 21:29:53 -0700

    get post titles from database to display on page

--- src/routes.php
+++ src/routes.php
@@ -14,40 +14,49 @@ return function (App $app) {
             $container->get('logger')->info("Slim-Skeleton '/' route");
 
             //connect to database
-            $container->get('db');
+            $db = $container->get('db');
+            $sql = "SELECT * FROM posts";
+            try{
+                $results = $db->query($sql);
+            } catch (Exception $e){
+                echo "Unable to retrieve results.";
+                exit;
+            }
+            $results->execute();
+            $args = $results->fetchAll(PDO::FETCH_ASSOC);
 
             // Render index view
             return $container->get('renderer')->render($response, 'index.phtml', $args);
         }
     );
-    $app->get(
-        '/blog/{id}',
-        function (Request $request, Response $response, array $args) use ($container) {
-            // Sample log message
-            $container->get('logger')->info("Slim-Skeleton '/' route");
+    // $app->get(
+    //     '/blog/{id}',
+    //     function (Request $request, Response $response, array $args) use ($container) {
+    //         // Sample log message
+    //         $container->get('logger')->info("Slim-Skeleton '/' route");
 
-            // Render index view
-            return $container->get('renderer')->render($response, 'detail.phtml', $args);
-        }
-    );
-    $app->get(
-        '/new',
-        function (Request $request, Response $response, array $args) use ($container) {
-            // Sample log message
-            $container->get('logger')->info("Slim-Skeleton '/' route");
+    //         // Render index view
+    //         return $container->get('renderer')->render($response, 'detail.phtml', $args);
+    //     }
+    // );
+    // $app->get(
+    //     '/new',
+    //     function (Request $request, Response $response, array $args) use ($container) {
+    //         // Sample log message
+    //         $container->get('logger')->info("Slim-Skeleton '/' route");
 
-            // Render index view
-            return $container->get('renderer')->render($response, 'new.phtml', $args);
-        }
-    );
-    $app->get(
-        '/edit',
-        function (Request $request, Response $response, array $args) use ($container) {
-            // Sample log message
-            $container->get('logger')->info("Slim-Skeleton '/' route");
+    //         // Render index view
+    //         return $container->get('renderer')->render($response, 'new.phtml', $args);
+    //     }
+    // );
+    // $app->get(
+    //     '/edit',
+    //     function (Request $request, Response $response, array $args) use ($container) {
+    //         // Sample log message
+    //         $container->get('logger')->info("Slim-Skeleton '/' route");
 
-            // Render index view
-            return $container->get('renderer')->render($response, 'edit.phtml', $args);
-        }
-    );
+    //         // Render index view
+    //         return $container->get('renderer')->render($response, 'edit.phtml', $args);
+    //     }
+    // );
 };
