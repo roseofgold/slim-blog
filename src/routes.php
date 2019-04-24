648c593f9a1385a3a78965a765cb7c567b7364d6
Melinda <roseofgold@gmail.com>
2019-04-24 11:12:01 -0700

    update edit entry route

--- src/routes.php
+++ src/routes.php
@@ -64,18 +64,6 @@ return function (App $app) {
         }
     );
 
-    // route to new blog entry
-    $app->get(
-        '/new',
-        function (Request $request, Response $response, array $args) use ($container) {
-            // Sample log message
-            $container->get('logger')->info("Slim-Skeleton '/' route");
-
-            // Render index view
-            return $container->get('renderer')->render($response, 'new.phtml', $args);
-        }
-    );
-
     // route to edit blog entry
     $app->get(
         '/edit/{id}',
@@ -85,9 +73,37 @@ return function (App $app) {
 
             // Get ID to aid is display of entry
             $id = $request->getAttribute('id');
+            
+            // connect to database
+            $db = $container->get('db');
+
+            // select specific post to display
+            $sql = "SELECT * FROM posts";
+            $sql .= " WHERE id = ?";
+            try {
+                $results = $db->prepare($sql);
+                $results->bindParam(1,$id);
+            } catch (Exception $e) {
+                echo "Unable to retrieve results: " . $e->getMessage();
+                exit;
+            }
+            $results->execute();
+            $args['posts'] = $results->fetch(PDO::FETCH_ASSOC);
 
             // Render index view
             return $container->get('renderer')->render($response, 'edit.phtml', $args);
+        }
+    );
+
+    // route to new blog entry
+    $app->get(
+        '/new',
+        function (Request $request, Response $response, array $args) use ($container) {
+            // Sample log message
+            $container->get('logger')->info("Slim-Skeleton '/' route");
+
+            // Render index view
+            return $container->get('renderer')->render($response, 'new.phtml', $args);
         }
     );
 };
